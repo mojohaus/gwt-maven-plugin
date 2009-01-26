2c6082d2a679e28b90ae4a606085de124b2b48ae
Olivier Lamy <olamy@apache.org>
2009-01-26 22:50:04 +0000

    remove not used method
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/gwt-maven-plugin@8942 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/gwt/shell/GWTTestMojo.java
+++ src/main/java/org/codehaus/mojo/gwt/shell/GWTTestMojo.java
@@ -23,15 +23,10 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
 
 import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.classworlds.ClassRealm;
-import org.codehaus.classworlds.ClassWorld;
 import org.codehaus.mojo.gwt.GwtRuntime;
 import org.codehaus.mojo.gwt.shell.scripting.ScriptUtil;
 import org.codehaus.mojo.gwt.shell.scripting.ScriptWriter;
@@ -180,39 +175,4 @@ public class GWTTestMojo
         return this.testSkip;
     }
 
-    /**
-     * Helper hack for classpath problems, used as a fallback.
-     * 
-     * @param runtime TODO
-     * 
-     * @return
-     */
-    protected ClassLoader fixThreadClasspath( GwtRuntime runtime )
-    {
-        try
-        {
-            ClassWorld world = new ClassWorld();
-
-            // use the existing ContextClassLoader in a realm of the classloading space
-            ClassRealm root = world.newRealm( "gwt-plugin", Thread.currentThread().getContextClassLoader() );
-            ClassRealm realm = root.createChildRealm( "gwt-project" );
-
-            Collection classpath = buildClasspathUtil.buildClasspathList( getProject(), Artifact.SCOPE_COMPILE,
-                                                                          runtime, sourcesOnPath, resourcesOnPath );
-            for ( Iterator it = classpath.iterator(); it.hasNext(); )
-            {
-                realm.addConstituent( ( (File) it.next() ).toURI().toURL() );
-            }
-
-            Thread.currentThread().setContextClassLoader( realm.getClassLoader() );
-            // /System.out.println("AbstractGwtMojo realm classloader = " + realm.getClassLoader().toString());
-
-            return realm.getClassLoader();
-        }
-        catch ( Exception e )
-        {
-            e.printStackTrace();
-            throw new RuntimeException( e );
-        }
-    }
 }
