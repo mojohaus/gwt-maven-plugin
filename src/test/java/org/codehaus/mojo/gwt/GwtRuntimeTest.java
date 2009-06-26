75a206856fcfd4ffa8c4fc0f117f111d5a392083
Nicolas De loof <nicolas.deloof@gmail.com>
2009-06-26 12:55:16 +0000

    refactoring broke the unit test on GWT SDK version detection
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/gwt-maven-plugin@10048 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/test/java/org/codehaus/mojo/gwt/GwtRuntimeTest.java
+++ src/test/java/org/codehaus/mojo/gwt/GwtRuntimeTest.java
@@ -2,6 +2,8 @@ package org.codehaus.mojo.gwt;
 
 import java.io.File;
 
+import org.apache.maven.plugin.MojoExecutionException;
+
 import junit.framework.TestCase;
 
 /**
@@ -14,28 +16,33 @@ public class GwtRuntimeTest
 {
     /**
      * Check version detection from GWT-dev Jar
+     * 
+     * @throws MojoExecutionException
      */
     public void testGwtVersion16M1Detection()
+        throws MojoExecutionException
     {
         File basedir = new File( System.getProperty( "basedir", "." ) );
-        File gwtDevJar = new File( basedir, "target/test-classes/gwt-dev-1.6.0-fake.jar" );
-        GwtRuntime gwt = new GwtRuntime( null, gwtDevJar );
+        File gwtHome = new File( basedir, "target/test-classes/fake-1.6.0" );
+        GwtRuntime gwt = new GwtRuntime( gwtHome );
         assertEquals( GwtVersion.ONE_DOT_SIX, gwt.getVersion() );
     }
 
     public void testGwtVersion153Detection()
+        throws MojoExecutionException
     {
         File basedir = new File( System.getProperty( "basedir", "." ) );
-        File gwtDevJar = new File( basedir, "target/test-classes/gwt-dev-1.5.3-fake.jar" );
-        GwtRuntime gwt = new GwtRuntime( null, gwtDevJar );
+        File gwtHome = new File( basedir, "target/test-classes/fake-1.5.3" );
+        GwtRuntime gwt = new GwtRuntime( gwtHome );
         assertEquals( GwtVersion.ONE_DOT_FIVE, gwt.getVersion() );
     }
 
     public void testGwtVersion1462Detection()
+        throws MojoExecutionException
     {
         File basedir = new File( System.getProperty( "basedir", "." ) );
-        File gwtDevJar = new File( basedir, "target/test-classes/gwt-dev-1.4.62-fake.jar" );
-        GwtRuntime gwt = new GwtRuntime( null, gwtDevJar );
+        File gwtHome = new File( basedir, "target/test-classes/fake-1.4.62" );
+        GwtRuntime gwt = new GwtRuntime( gwtHome );
         assertEquals( GwtVersion.ONE_DOT_FOUR, gwt.getVersion() );
     }
 
