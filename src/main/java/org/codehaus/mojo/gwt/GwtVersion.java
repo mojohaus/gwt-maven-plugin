d9b21db91d8e2231a6caf7b9ad4de0953191d4ce
Nicolas De loof <nicolas.deloof@gmail.com>
2010-02-05 15:18:12 +0000

    code cleanup : use bitmask for GWT features to avoid unreadable multi-boolean constructor
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/gwt-maven-plugin@11808 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/gwt/GwtVersion.java
+++ src/main/java/org/codehaus/mojo/gwt/GwtVersion.java
@@ -19,73 +19,73 @@ package org.codehaus.mojo.gwt;
  * under the License.
  */
 
+import static org.codehaus.mojo.gwt.EmbeddedServer.JETTY;
+import static org.codehaus.mojo.gwt.EmbeddedServer.TOMCAT;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.JAVA5_SYNTAX;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.MULTI_MODULE_COMPILER;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.MULTI_MODULE_SHELL;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.OOPHM;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.PARALLEL_COMPILER;
+import static org.codehaus.mojo.gwt.GwtVersion.Capabilities.SOYC;
+
 /**
  * @author <a href="mailto:nicolas@apache.org">Nicolas De Loof</a>
  */
 public enum GwtVersion
 {
-    ONE_DOT_FOUR( false, "com.google.gwt.dev.GWTShell", "com.google.gwt.dev.GWTCompiler", EmbeddedServer.TOMCAT,
-                  false, false, false, false, false, "-out" )
+    ONE_DOT_FOUR( 0, 
+        "com.google.gwt.dev.GWTShell", "com.google.gwt.dev.GWTCompiler", TOMCAT, "-out" )
 
-    , ONE_DOT_FIVE( true, "com.google.gwt.dev.GWTShell", "com.google.gwt.dev.GWTCompiler", EmbeddedServer.TOMCAT,
-                    false, false, false, false, false, "-out" )
+    , ONE_DOT_FIVE( ONE_DOT_FOUR.capabilities + JAVA5_SYNTAX,
+        "com.google.gwt.dev.GWTShell", "com.google.gwt.dev.GWTCompiler", TOMCAT, "-out" )
 
-    , ONE_DOT_SIX( true, "com.google.gwt.dev.HostedMode", "com.google.gwt.dev.Compiler", EmbeddedServer.JETTY,
-                   true, false, true, false, false, "-war" )
+    , ONE_DOT_SIX( ONE_DOT_FIVE.capabilities + PARALLEL_COMPILER + MULTI_MODULE_COMPILER,
+        "com.google.gwt.dev.HostedMode", "com.google.gwt.dev.Compiler", JETTY, "-war" )
 
-    , ONE_DOT_SEVEN( true, "com.google.gwt.dev.HostedMode", "com.google.gwt.dev.Compiler", EmbeddedServer.JETTY,
-                   true, false, true, false, false, "-war" )
+    , ONE_DOT_SEVEN( ONE_DOT_SIX.capabilities,
+        "com.google.gwt.dev.HostedMode", "com.google.gwt.dev.Compiler", JETTY, "-war" )
 
-    , TWO_DOT_ZERO( true, "com.google.gwt.dev.DevMode", "com.google.gwt.dev.Compiler", EmbeddedServer.JETTY,
-                    true, true, true, true, true, "-war" )
-    , FUTURE( true, "com.google.gwt.dev.HostedMode", "com.google.gwt.dev.Compiler", EmbeddedServer.JETTY,
-              true, true, true, true, false, "-war" );
+    , TWO_DOT_ZERO( ONE_DOT_SEVEN.capabilities + MULTI_MODULE_SHELL + SOYC + OOPHM,
+        "com.google.gwt.dev.DevMode", "com.google.gwt.dev.Compiler", JETTY, "-war" );
 
 
     static GwtVersion fromMavenVersion( String version )
-            {
-                if ( version.startsWith( "1.4" ) )
-                {
-                    return ONE_DOT_FOUR;
-                }
-                if ( version.startsWith( "1.5" ) )
-                {
-                    return ONE_DOT_FIVE;
-                }
-                if ( version.startsWith( "1.6" ) )
-                {
-                    return ONE_DOT_SIX;
-                }
-                if ( version.startsWith( "1.7" ) )
-                {
-                    return ONE_DOT_SEVEN;
-                }
-                if ( version.startsWith( "2.0" ) )
-                {
-                    return TWO_DOT_ZERO;
-                }
-                return FUTURE;
-            }
-
-    private GwtVersion( boolean supportJava5, String shellFQCN, String compilerFQCN,
-                        EmbeddedServer emebededServer,
-                        boolean supportMultiModuleCompiler, boolean supportMultiModuleShell, 
-                        boolean supportParallelCompiler,
-                        boolean supportSOYC, boolean supportOOPHM, String webOutputArgument )
     {
-        this.supportJava5 = supportJava5;
+        if ( version.startsWith( "1.4" ) )
+        {
+            return ONE_DOT_FOUR;
+        }
+        if ( version.startsWith( "1.5" ) )
+        {
+            return ONE_DOT_FIVE;
+        }
+        if ( version.startsWith( "1.6" ) )
+        {
+            return ONE_DOT_SIX;
+        }
+        if ( version.startsWith( "1.7" ) )
+        {
+            return ONE_DOT_SEVEN;
+        }
+        if ( version.startsWith( "2.0" ) )
+        {
+            return TWO_DOT_ZERO;
+        }
+        throw new IllegalStateException( "Unsupported GWT version " + version );
+    }
+
+    private GwtVersion( int capabilities, String shellFQCN, String compilerFQCN,
+                        EmbeddedServer emebededServer,
+        String webOutputArgument )
+    {
+        this.capabilities = capabilities;
         this.shellFQCN = shellFQCN;
         this.compilerFQCN = compilerFQCN;
-        this.supportMultiModuleCompiler = supportMultiModuleCompiler;
-        this.supportMultiModuleShell = supportMultiModuleShell;
-        this.supportParallelCompiler = supportParallelCompiler;
-        this.supportSOYC = supportSOYC;
-        this.supportOOPHM = supportOOPHM;
         this.webOutputArgument = webOutputArgument;
         this.emebededServer = emebededServer;
     }
 
-    private boolean supportJava5;
+    private int capabilities;
 
     private String shellFQCN;
 
@@ -93,22 +93,8 @@ public enum GwtVersion
 
     private EmbeddedServer emebededServer;
 
-    private boolean supportMultiModuleCompiler;
-    
-    private boolean supportMultiModuleShell;
-
-    private boolean supportParallelCompiler;
-
-    private boolean supportSOYC;
-    
-    private boolean supportOOPHM;
-
     private String webOutputArgument;
 
-    public boolean supportJava5()
-    {
-        return supportJava5;
-    }
 
     /**
      * @return fully qualified class name of the GWTShell "main" class
@@ -128,33 +114,53 @@ public enum GwtVersion
         return emebededServer;
     }
 
+    public boolean supportJava5()
+    {
+        return ( capabilities & JAVA5_SYNTAX ) != 0;
+    }
+
     public boolean supportMultiModuleCompile()
     {
-        return supportMultiModuleCompiler;
+        return ( capabilities & MULTI_MODULE_COMPILER ) != 0;
     }
 
     public boolean supportMultiModuleShell()
     {
-        return supportMultiModuleShell;
+        return ( capabilities & MULTI_MODULE_SHELL ) != 0;
     }
 
     public boolean supportParallelCompile()
     {
-        return supportParallelCompiler;
+        return ( capabilities & PARALLEL_COMPILER ) != 0;
     }
 
     public boolean supportSOYC()
     {
-        return supportSOYC;
+        return ( capabilities & SOYC ) != 0;
     }
     
     public boolean supportOOPHM()
     {
-        return supportOOPHM;
+        return ( capabilities & OOPHM ) != 0;
     }
 
     public String getWebOutputArgument()
     {
         return webOutputArgument;
+    }
+
+    static final class Capabilities
+    {
+        static final int JAVA5_SYNTAX = 1;
+
+        static final int PARALLEL_COMPILER = 2;
+
+        static final int MULTI_MODULE_SHELL = 4;
+
+        static final int MULTI_MODULE_COMPILER = 8;
+
+        static final int SOYC = 16;
+
+        static final int OOPHM = 32;
     }
 }
